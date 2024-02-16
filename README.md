b9a54a34546a0424a21768f6784b74e1fab5acb8
Romain VIGNARD <romain78780@gmail.com>
2024-02-16 16:11:43 +0100

    second commit

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# TP-Flask_SQLAlchemy_VIGNARD_ROMAIN
+Application de Réservation d'Hôtels
+
+
+Description
+
+Cette application de réservation d'hôtels permet aux utilisateurs de rechercher des chambres disponibles, de créer des réservations, et d'annuler des réservations existantes.
+
+
+Routes
+
+Ajouter des chambres : '/api/chambres'
+Modifier des chambres : '/api/chambres/<int:id>' --> PUT 
+Supprimer des chambres : '/api/chambres/<int:id>' --> DELETE
+Recherche de Chambres Disponibles : '/api/chambres/disponibles'
+Création de Réservation : '/api/reservations'
+Annulation de Réservation : '/api/reservations/<int:id>' --> DELETE
+
+
+
+Technologies Utilisées
+
+Flask, SQLAlchemy, SQLite 
--- src/reservation_hotel/routes.py
+++ src/reservation_hotel/routes.py
@@ -1,31 +1,30 @@
 from .database import db
 from .models import  Chambre, Reservation
-from flask import Flask, request, jsonify, Blueprint
+from flask import Flask, request, jsonify, Blueprint, render_template
 main = Blueprint('main', __name__)
 from datetime import datetime
 from sqlalchemy import and_, or_
 
+@main.route('/')
+def index():
+    return render_template('index.html')
+    
 @main.route('/api/chambres', methods=['POST'])
 def ajouter_chambre():
     try:
-        # Extraction des données de la requête
         data = request.get_json()
         numero = data['numero']
-        type_chambre = data['type']
+        type = data['type']
         prix = data['prix']
 
-        # Vérification de l'unicité du numéro de chambre
         if Chambre.query.filter_by(numero=numero).first():
             return jsonify({"success": False, "message": "Une chambre avec ce numéro existe déjà."}), 400
 
-        # Création de la nouvelle chambre
-        nouvelle_chambre = Chambre(numero=numero, type=type_chambre, prix=prix)
+        nouvelle_chambre = Chambre(numero=numero, type=type, prix=prix)
 
-        # Ajout de la chambre à la base de données
         db.session.add(nouvelle_chambre)
         db.session.commit()
 
-        # Réponse de succès
         return jsonify({"success": True, "message": "Chambre ajoutée avec succès."}), 201
     except Exception as e:
         db.session.rollback()
@@ -33,7 +32,6 @@ def ajouter_chambre():
 
 @main.route('/api/chambres/<int:id>', methods=['PUT'])
 def modifier_chambre(id):
-    # Récupération de la chambre par son ID
     chambre = Chambre.query.get(id)
     
     if not chambre:
@@ -41,28 +39,23 @@ def modifier_chambre(id):
 
     data = request.get_json()
     
-    # Mise à jour des attributs de la chambre avec les nouvelles valeurs
     chambre.numero = data.get('numero', chambre.numero)
     chambre.type = data.get('type', chambre.type)
     chambre.prix = data.get('prix', chambre.prix)
     
-    # Enregistrement des modifications dans la base de données
     db.session.commit()
     
     return jsonify({"success": True, "message": "Chambre mise à jour avec succès."})   
 
 @main.route('/api/chambres/<int:id>', methods=['DELETE'])
 def supprimer_chambre(id):
-    # Récupération de la chambre par son ID
     chambre = Chambre.query.get(id)
     
     if not chambre:
         return jsonify({"success": False, "message": "Chambre non trouvée."}), 404
     
-    # Suppression de la chambre
     db.session.delete(chambre)
     
-    # Enregistrement de la suppression dans la base de données
     db.session.commit()
     
     return jsonify({"success": True, "message": "Chambre supprimée avec succès."})
@@ -76,7 +69,6 @@ def creer_reservation():
     date_arrivee = datetime.strptime(data['date_arrivee'], '%Y-%m-%d').date()
     date_depart = datetime.strptime(data['date_depart'], '%Y-%m-%d').date()
     
-    # Vérification de la disponibilité de la chambre
     reservations_existantes = Reservation.query.filter(
         Reservation.id_chambre == id_chambre,
         Reservation.date_depart > date_arrivee,
@@ -86,7 +78,6 @@ def creer_reservation():
     if reservations_existantes:
         return jsonify({"success": False, "message": "La chambre n'est pas disponible pour les dates demandées."}), 400
     
-    # Création de la nouvelle réservation
     nouvelle_reservation = Reservation(
         id_client=id_client,
         id_chambre=id_chambre,
@@ -101,7 +92,6 @@ def creer_reservation():
 
 @main.route('/api/reservations/<int:id>', methods=['DELETE'])
 def annuler_reservation(id):
-    # Récupération de la réservation par son ID
     reservation = Reservation.query.get(id)
     
     if not reservation:
@@ -117,7 +107,6 @@ def rechercher_chambres_disponibles():
     date_arrivee_str = request.args.get('date_arrivee')
     date_depart_str = request.args.get('date_depart')
 
-    # Vérifie si les paramètres 'date_arrivee' et 'date_depart' sont présents et non vides
     if not date_arrivee_str or not date_depart_str:
         return jsonify({"error": "Les paramètres 'date_arrivee' et 'date_depart' sont requis et doivent être non vides."}), 400
 
@@ -125,10 +114,8 @@ def rechercher_chambres_disponibles():
         date_arrivee = datetime.strptime(date_arrivee_str, '%Y-%m-%d').date()
         date_depart = datetime.strptime(date_depart_str, '%Y-%m-%d').date()
     except ValueError:
-        # Gère le cas où la conversion de la date échoue
         return jsonify({"error": "Format de date invalide. Utilisez le format 'AAAA-MM-JJ'."}), 400
 
-    # Requête pour trouver les chambres disponibles
     chambres_disponibles = Chambre.query.filter(
         ~Chambre.reservations.any(
             or_(
@@ -142,7 +129,6 @@ def rechercher_chambres_disponibles():
         )
     ).all()
 
-    # Formatage des résultats
     resultat = [
         {
             "id": chambre.id,
